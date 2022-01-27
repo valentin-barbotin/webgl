68c290c1faf39e065452d8699c2df2d23b2828ae
Valentin Barbotin <valentin.barbotin@sixense-group.com>
2022-01-27 17:18:58 +0100

    fix: tmtc

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable no-underscore-dangle */
 /* eslint-disable no-plusplus */
 /* eslint-disable no-restricted-syntax */
@@ -7,44 +8,43 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import dat from 'dat.gui';
 import { Vector3 } from 'three';
-import { objectToBuffer, BufferToObject, ws, Message } from './ws';
-import { GUI } from 'dat.gui';
-import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader.js';
-
-
-
+import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader';
+// import { objectToBuffer, BufferToObject, ws, Message } from './ws';
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-function connected() {
-  console.log('Connected to server ...');
-  if (ws.readyState === ws.OPEN) {
-    const response: Message = {
-      type: 'getAll',
-      data: 'welcome ok',
-    };
-    const payload = objectToBuffer(response);
-    ws.send(payload);
-  }
-}
-ws.onopen = connected;
+const loader = new THREE.TextureLoader();
+const marbleMaterial = new THREE.MeshPhongMaterial({ map: loader.load('/dist/assets/marbre.jpg') });
 
-async function sendMessageToWS() {
-  console.log(ws.OPEN);
-  // if (ws.readyState === ws.OPEN) {
-  //   const response: Message = {
-  //     type: 'getAll',
-  //     data: 'welcome ok',
-  //   };
-  //   const payload = objectToBuffer(response);
-  //   ws.send(payload);
-  // }
-}
+// function connected() {
+//   console.log('Connected to server ...');
+//   if (ws.readyState === ws.OPEN) {
+//     const response: Message = {
+//       type: 'getAll',
+//       data: 'welcome ok',
+//     };
+//     const payload = objectToBuffer(response);
+//     ws.send(payload);
+//   }
+// }
+// ws.onopen = connected;
+
+// async function sendMessageToWS() {
+//   console.log(ws.OPEN);
+//   // if (ws.readyState === ws.OPEN) {
+//   //   const response: Message = {
+//   //     type: 'getAll',
+//   //     data: 'welcome ok',
+//   //   };
+//   //   const payload = objectToBuffer(response);
+//   //   ws.send(payload);
+//   // }
+// }
 
 function createAPillar(buildingHeigth: number, scene: THREE.Scene) {
   const [topRadius, bottomRadius] = [2, 2];
   const geometry = new THREE.CylinderGeometry(topRadius, bottomRadius, buildingHeigth);
-  const material = new THREE.MeshPhongMaterial({ color: 0xff0000 });
+  const material = marbleMaterial.clone();
   const piller = new THREE.Mesh(geometry, material);
   piller.receiveShadow = true;
   piller.castShadow = true;
@@ -74,7 +74,6 @@ const buildingHeigth = 22;
 const space = -(buildingLength * 0.18) + pillarDiameter * 3;
 
 const scene = new THREE.Scene();
-const loader = new THREE.TextureLoader();
 const controls = new OrbitControls(camera, renderer.domElement); // add the ability to move the camera around the scene
 
 const light = new THREE.DirectionalLight(0xffffff, 1);
@@ -118,14 +117,14 @@ camera.position.y = 20;
 camera.position.z = 60;
 
 // box got 6 faces, so 6 materials
+
 const materials = [
-  // new THREE.MeshPhongMaterial({ map: loader.load('dist/assets/marbre.jpg') }),
-  new THREE.MeshPhongMaterial({ map: loader.load('https://threejsfundamentals.org/threejs/resources/images/flower-1.jpg') }),
-  new THREE.MeshPhongMaterial({ map: loader.load('https://threejsfundamentals.org/threejs/resources/images/flower-2.jpg') }),
-  new THREE.MeshPhongMaterial({ map: loader.load('https://threejsfundamentals.org/threejs/resources/images/flower-3.jpg') }),
-  new THREE.MeshPhongMaterial({ map: loader.load('https://threejsfundamentals.org/threejs/resources/images/flower-4.jpg') }),
-  new THREE.MeshPhongMaterial({ map: loader.load('https://threejsfundamentals.org/threejs/resources/images/flower-5.jpg') }),
-  new THREE.MeshPhongMaterial({ map: loader.load('https://threejsfundamentals.org/threejs/resources/images/flower-6.jpg') }),
+  marbleMaterial.clone(),
+  marbleMaterial.clone(),
+  marbleMaterial.clone(),
+  marbleMaterial.clone(),
+  marbleMaterial.clone(),
+  marbleMaterial.clone(),
 ];
 
 if (!materials) {
@@ -145,9 +144,7 @@ floor.castShadow = true;
 // create a box
 const roof = new THREE.Mesh(
   new THREE.BoxGeometry(buildingLength, 2, buildingLength * 0.3),
-  new THREE.MeshPhongMaterial({
-    map: loader.load('https://threejsfundamentals.org/threejs/resources/images/wall.jpg'),
-  }),
+  marbleMaterial.clone(),
 );
 
 // define the position of the ceil in the scene, higher than the floor
@@ -160,9 +157,7 @@ scene.add(roof);
 
 const ground = new THREE.Mesh(
   new THREE.BoxGeometry(buildingLength * 10, 1, buildingLength * 10),
-  new THREE.MeshPhongMaterial({
-    color: 0xDDDDDD,
-  }),
+  marbleMaterial.clone(),
 );
 ground.receiveShadow = true;
 ground.castShadow = true;
@@ -179,20 +174,14 @@ const cameraHelper = new THREE.CameraHelper(light.shadow.camera);
 scene.add(lightHelper);
 scene.add(cameraHelper);
 
-//Add chest
+// Add chest
 const chest = new THREE.Mesh(
   new THREE.BoxGeometry(10, 10, 10),
-  new THREE.MeshPhongMaterial({
-    map: loader.load('https://threejsfundamentals.org/threejs/resources/images/flower-1.jpg'),
-  }),
+  marbleMaterial.clone(),
 );
 chest.position.set(0, 7, 0);
 chest.receiveShadow = true;
 chest.castShadow = true;
-// const textureCube = new THREE.TextureLoader().load( 'https://threejsfundamentals.org/threejs/resources/images/flower-1.jpg' );
-// const geometryCube = new THREE.BoxGeometry( 10, 10, 10 );
-// const materialCube = new THREE.MeshBasicMaterial( { map: textureCube } );
-// const meshCube = new THREE.Mesh( geometryCube, materialCube );
 scene.add(chest);
 
 function addVecToMenu(gui: dat.GUI, vec: Vector3, name: string) {
@@ -202,64 +191,58 @@ function addVecToMenu(gui: dat.GUI, vec: Vector3, name: string) {
   folder.add(vec, 'z', -100, 100);
 }
 
-
-
 // const gui = new dat.GUI();
 // gui.addFolder('Light');
 // gui.add(light, 'intensity', 0, 1);
 // gui.add(light, 'castShadow');
-const geometry = new THREE.BoxGeometry()
+const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial({
   color: 0x00ff00,
   wireframe: true,
-})
-const cube = new THREE.Mesh(geometry, material)
-scene.add(cube)
+});
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
 
-const gui = new GUI()
+const gui = new dat.GUI();
 // const cubeFolder = gui.addFolder('Cube')
 // cubeFolder.add(cube.rotation, 'x', 0, 100)
 // cubeFolder.add(cube.rotation, 'y', 0, 100)
 // cubeFolder.add(cube.rotation, 'z', 0, 100)
 // cubeFolder.open()
-const cameraFolder = gui.addFolder('Camera')
-cameraFolder.add(camera.position, 'z', -100, 100)
-cameraFolder.open()
+const cameraFolder = gui.addFolder('Camera');
+cameraFolder.add(camera.position, 'z', -100, 100);
+cameraFolder.open();
 
 addVecToMenu(gui, light.position, 'Position');
 addVecToMenu(gui, light.target.position, 'Target');
 
-var objs: { object: THREE.Group; mixer: THREE.AnimationMixer; }[] = []           
-var loaderCharacter = new FBXLoader();
-loaderCharacter.load("/dist/assets/Capoeira.fbx", function (object) {
+const mixers: THREE.AnimationMixer[] = [];
+const loaderCharacter = new FBXLoader();
+
+loaderCharacter.load('/dist/assets/Capoeira.fbx', (object) => {
   if (!object) {
-    console.log("Character not found");
+    console.log('Character not found');
     return;
-  };
+  }
 
   object.scale.set(0.05, 0.05, 0.05);
   object.position.set(20, 1, 0);
   const mixer = new THREE.AnimationMixer(object);
-  var action = mixer.clipAction(object.animations[0]);
-
-  console.log(action);
+  const action = mixer.clipAction(object.animations[0]);
 
   action.play();
   scene.add(object);
-  objs.push({object, mixer});
-})
+  mixers.push(mixer);
+});
 
-const clock = new THREE.Clock();  
+const clock = new THREE.Clock();
 
-(function animate() {
-  objs.forEach(({mixer}) => {mixer.update(clock.getDelta());});
-  renderer.render(scene, camera);
-  requestAnimationFrame(animate);
-})();
-
-sendMessageToWS();
+// sendMessageToWS();
 
 (function animate() {
+  for (const mixer of mixers) {
+    mixer.update(clock.getDelta());
+  }
   requestAnimationFrame(animate);
   light.target.updateMatrixWorld();
   light.shadow.camera.updateProjectionMatrix();
